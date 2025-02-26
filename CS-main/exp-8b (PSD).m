5b47684d0bb89a4636dfa795a5bd383353abc20c
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2025-02-26 21:21:11 +0530

    Update exp-8b (PSD).m

--- CS-main/exp-8b (PSD).m
+++ CS-main/exp-8b (PSD).m
@@ -5,20 +5,17 @@ f_zero = 1e-9;
 
 % Unipolar NRZ
 s_unipolar_nrz = (v^2 * T/4) * (sin(pi * f * T) ./ (pi * f * T)).^2;
-s_unipolar_nrz(1) = s_unipolar_nrz(1) + (v^2 / 4) + f_zero;
-
-figure;
-stem(f_zero, s_unipolar_nrz(1), 'or', 'LineWidth', 2, 'MarkerSize', 8);
-hold on;
 plot(f, s_unipolar_nrz, '-r', 'LineWidth', 2);
+
+% Polar NRZ
+s_polar_nrz = (v^2 * T) * (sin(pi * f * T) ./ (pi * f * T)).^2;
+plot(f, s_polar_nrz, '--b', 'LineWidth', 2);
+
 
 % Manchester code
 s_manchester = (v^2 * T) * ((sin(pi * f * T/2) ./ (pi * f * T/2)).^2) .* (sin(pi * f * T/2).^2);
 plot(f, s_manchester, '--g', 'LineWidth', 2);
 
-% Polar NRZ
-s_polar_nrz = (v^2 * T) * (sin(pi * f * T) ./ (pi * f * T)).^2;
-plot(f, s_polar_nrz, '--b', 'LineWidth', 2);
 
 % Bipolar RZ
 s_bipolar_rz = (v^2 * T/4) * ((sin(pi * f * T/2) ./ (pi * f * T/2)).^2) .* (sin(pi * f * T).^2);
